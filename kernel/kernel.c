8a56b9a6362aaa1289a4cf320f56f4e952e3dfa5
Theo Lincke <lincketheo@gmail.com>
2023-04-10 23:19:43 -0600

    removed lying comment

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -39,9 +39,6 @@ void printString(const char* str) {
 
 /**
  * @brief The entry point into the kernel
- *
- * Currently, this sets the memory address 0xb8000 to Q
- * Memory address 0xb8000 is the start of video memory
  */
 extern void main() {
     const char data[] = "Hello World!";
